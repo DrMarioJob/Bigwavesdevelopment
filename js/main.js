300258e2c3a2d93b1c35cdaf918b7046aab90854
disco1988 <prowebmasti@gmail.com>
2024-12-08 14:50:44 +0200

    Add Contacts page and Edits

--- js/main.js
+++ js/main.js
@@ -3,3 +3,23 @@ document.querySelector('.mobileHeadBtn').addEventListener('click', () => {
     document.querySelector('.navigation').classList.toggle('active');
     document.querySelector('.mobileHeadBtn').classList.toggle('active');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.sec'); // Все блоки с классом .sec
+
+    const handleScroll = () => {
+        const triggerBottom = window.innerHeight / 1.2; // Точка срабатывания
+
+        sections.forEach((section) => {
+            const sectionTop = section.getBoundingClientRect().top;
+
+            if (sectionTop < triggerBottom) {
+                section.classList.add('visible'); // Добавляем класс видимости
+            }
+        });
+    };
+
+    window.addEventListener('scroll', handleScroll); // Слушаем прокрутку
+
+    handleScroll(); // Проверяем начальное положение
+});
