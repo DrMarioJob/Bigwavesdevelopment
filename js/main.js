19690e267dc5c09393caae36c351203e8b59a269
disco1988 <prowebmasti@gmail.com>
2024-12-15 07:15:18 +0200

    Edit forms

--- js/main.js
+++ js/main.js
@@ -24,3 +24,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
     handleScroll();
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const formGroups = document.querySelectorAll('.formGroupContainer');
+
+    formGroups.forEach(container => {
+        const mediaInputs = container.querySelectorAll('.mediaInput');
+        const usernameField = container.querySelector('.usernameField');
+        const formGroupGrid2 = container.querySelector('.formGroupGrid2'); // Находим элемент для добавления класса
+
+        if (mediaInputs.length > 0 && usernameField) {
+            mediaInputs.forEach(input => {
+                input.addEventListener('change', function () {
+                    if (this.value === 'Telegram' && this.checked) {
+                        usernameField.style.display = 'flex';
+                        if (formGroupGrid2) {
+                            formGroupGrid2.classList.add('active'); // Добавляем класс
+                        }
+                    } else {
+                        usernameField.style.display = 'none';
+                        if (formGroupGrid2) {
+                            formGroupGrid2.classList.remove('active'); // Удаляем класс
+                        }
+                    }
+                });
+            });
+        }
+    });
+});
